765a2d222ea08e8acbff8a003c145b9f3976ff05
VruttikBhatt <vruttik.bhatt@gmail.com>
2019-08-14 19:37:27 -0500

    mysite repo is updated

---
+++ js/index107.js
@@ -0,0 +1,5 @@
+function biggerNumber(firstNumber=3, secondNumber=5){
+    return firstNumber+secondNumber;
+}
+console.log(`Number ${firstNumber} is bigger than ${secondNumber}`);
+
---
+++ js/index117.js
@@ -0,0 +1,9 @@
+const people= ['Tom', 'Jerry', 'Ignacio', 'Harry', 'Rick','Eric'];
+
+
+console.log(people[1]);
+    console.log(people[3]);
+        console.log(people[5]);    
+
+
+
